7beee16d33f2f0090b643ba2139187f973909220
Johnson adeola <johnsamxy@gmail.com>
2023-12-25 18:52:56 +0100

    added documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,15 @@
+""" This is a flask module """
+
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+        This function defines the view for the '/' url
+    """
     return "Hello HBNB!"
 
 
