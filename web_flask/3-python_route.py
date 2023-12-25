cf042bbb466982c72fa9311861a6841d9d6b60b8
Johnson adeola <johnsamxy@gmail.com>
2023-12-25 21:09:00 +0100

    new task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" This is a flask app module """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+        This function handles the home view
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        This function handles the HBNB view
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cDisplay(text):
+    """
+        This functions handles the C view
+    """
+    newtext = text.replace('_', ' ')
+    return "C {}".format(newtext)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyDisplay(text='is cool'):
+    """
+        This function handles the python display
+    """
+    if '_' in text:
+        newtext = text.replace('_', ' ')
+        return "python {}".format(newtext)
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
