e3629337f89095353aeb9068f3423430ffa9f631
Johnson adeola <johnsamxy@gmail.com>
2023-12-26 12:37:37 +0100

    fixed pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -55,9 +55,11 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     if (n % 2) == 0:
-        return render_template('6-number_odd_or_even.html', num=n, num_type='even')
+        return render_template('6-number_odd_or_even.html',
+                               num=n, num_type='even')
     if (n % 2) != 0:
-        return render_template('6-number_odd_or_even.html', num=n, num_type='odd')
+        return render_template('6-number_odd_or_even.html',
+                               num=n, num_type='odd')
 
 
 if __name__ == '__main__':
