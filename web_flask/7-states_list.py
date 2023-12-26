5d26239cbcd393ea0fe73f89cab7023c3b52ae67
Johnson adeola <johnsamxy@gmail.com>
2023-12-26 17:47:00 +0100

    new task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module is a scripts that starts a flask web app """
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_db():
+    """ This function handles closing of the db """
+    storage.close()
+
+
+@app.route('/states_list')
+def state():
+    """ This function parses the state objects """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
