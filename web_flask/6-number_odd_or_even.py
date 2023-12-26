9e9b1399ddbf3d7e08c145ac8a65d622cd3fd78e
Johnson adeola <johnsamxy@gmail.com>
2023-12-26 12:36:56 +0100

    new task

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -52,5 +52,13 @@ def number_template(n):
     return render_template('5-number.html', num=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    if (n % 2) == 0:
+        return render_template('6-number_odd_or_even.html', num=n, num_type='even')
+    if (n % 2) != 0:
+        return render_template('6-number_odd_or_even.html', num=n, num_type='odd')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
